ec51dfe8197dc71c2e165e064f03f96bea6b6137
chamarasab <chamara.sab@outlook.com>
2026-02-22 11:02:54 +0530

    Generate users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://chamara:6526560@crudcluster.6pfkbzp.mongodb.net/crud_db?appName=CRUDCluster')
+    MongooseModule.forRoot('mongodb+srv://chamara:6526560@crudcluster.6pfkbzp.mongodb.net/crud_db?appName=CRUDCluster'),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
