55595da160053bfafa3f2e4f6a6ede5869ada51d
chamarasab <chamara.sab@outlook.com>
2026-02-22 11:29:28 +0530

    Add users service

---
+++ src/users/users.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schema/users.schema';
+
+@Injectable()
+export class UsersService {
+    constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+    async getAllUsers(): Promise<User[]> {
+        return this.userModel.find().exec();
+    }
+
+    async createUser(user: User): Promise<User> {
+        const createdUser = new this.userModel(user);
+        return createdUser.save();
+    }
+
+    async getUserById(id: string): Promise<User | null> {
+        return this.userModel.findById(id).exec();
+    }
+
+    async updateUser(id: string, user: Partial<User>): Promise<User | null> {
+        return this.userModel.findByIdAndUpdate(id, user, { new: true }).exec();
+    }
+
+    async deleteUser(id: string): Promise<User | null> {
+        return this.userModel.findByIdAndDelete(id).exec();
+    }   
+}
