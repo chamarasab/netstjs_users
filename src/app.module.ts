cf2d2b2f9cfeace1c189a18fe5d8453804457d5d
chamarasab <chamara.sab@outlook.com>
2026-02-22 10:58:34 +0530

    Set up MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://chamara:6526560@crudcluster.6pfkbzp.mongodb.net/crud_db?appName=CRUDCluster')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
