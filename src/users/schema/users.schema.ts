e5978100c00efb9e8fbdd2bf9c121ddd4ef41363
chamarasab <chamara.sab@outlook.com>
2026-02-22 11:11:02 +0530

    Update users schema

--- src/users/schema/users.schema.ts
+++ src/users/schema/users.schema.ts
@@ -1,1 +1,22 @@
-export class UsersSchema {}
+import {Prop, Schema, SchemaFactory} from "@nestjs/mongoose";
+import {Document} from "mongoose";
+import { version } from "os";
+
+export type UserDocument = User & Document;
+
+@Schema({ versionKey: false })
+export class User {
+    @Prop({required: true})
+    name: string;
+
+    @Prop({required: true})
+    email: string;
+
+    @Prop({required: true})
+    password: string;
+
+    @Prop({required: true})
+    created_at: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
